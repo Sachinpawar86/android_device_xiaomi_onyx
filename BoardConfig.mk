c455aae67d38acd74b20bdf532ced172725f0a14
Sachinpawar86 <sachinbarange86@gmail.com>
2025-12-03 13:09:45 +0530

    onyx: Move device_framework_matrix.xml to hardware/lineage/interfaces Lunaris-AOSP/vendor_lineage@2d0bf86
    Signed-off-by: Sachin <sachinbarange86@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,10 +69,9 @@ HWUI_COMPILE_FOR_PERF := true
 TARGET_USES_VULKAN := true
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/hidl/compatibility_matrix.device.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
 
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix_aidl.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
