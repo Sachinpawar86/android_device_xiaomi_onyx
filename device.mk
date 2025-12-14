ce5dd7c8d8b80313e56a7b7960c3fc6f3d9b0ddb
romgharti <romgharti@gmail.com>
2025-12-14 15:38:10 +0000

    onyx: perf: Import powerhint.xml from stock rom
    Signed-off-by: romgharti <romgharti@gmail.com>

--- device.mk
+++ device.mk
@@ -316,7 +316,8 @@ PRODUCT_PACKAGES += \
 # Perf
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
-    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml 
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml \
+    $(LOCAL_PATH)/configs/perf/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -327,9 +328,6 @@ $(call soong_config_set,qtipower,mode_ext_lib,//$(LOCAL_PATH):libpowermode-ext-o
 
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
-
-PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/sun/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # Properties
 PRODUCT_COPY_FILES += \
