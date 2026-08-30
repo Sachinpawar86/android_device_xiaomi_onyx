189a09634f3b39fd8819ce99faa6bf735891e419
romgharti <romgharti@gmail.com>
2026-08-30 11:02:31 +0000

    onyx: Force 'speed' compiler filter for SystemServer and SystemUI
    Set dalvik.vm.systemservercompilerfilter and dalvik.vm.systemuicompilerfilter
    to 'speed' to force full AOT compilation for core system components.
    
    This improves UI responsiveness, eliminates initial JIT stutters, and lowers
    runtime CPU overhead after first boot.
    
    Signed-off-by: romgharti <romgharti@gmail.com>
    Signed-off-by: Sachin <sachinbarange86@gmail.com>

--- device.mk
+++ device.mk
@@ -139,6 +139,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Dex2oat (Compile SystemServer & SystemUI on device with `speed`)
+PRODUCT_SYSTEM_PROPERTIES += \
+    dalvik.vm.systemservercompilerfilter=speed \
+    dalvik.vm.systemuicompilerfilter=speed
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
